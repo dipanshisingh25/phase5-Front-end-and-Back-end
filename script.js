02705c695a23cbdfc3e002bcad09fdeaeb9a5b7e
dipanshi Singh <dipanshisingh25@navgurukul.org>
2026-08-17 22:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById("btn");
+const inp = document.querySelector(".inp");
+const output = document.querySelector(".output");
+
+btn.addEventListener("click", async () => {
+
+    const input = inp.value;
+
+    if (input === "") {
+        alert("Fill the box");
+        return;
+    }
+
+    output.innerText = "Working...";
+
+    try {
+        const response = await fetch("http://localhost:5000/ask", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ input })
+        });
+
+        const data = await response.json();
+
+        output.innerText = data.output;
+
+    } catch (error) {
+        console.error(error);
+        output.innerText = "Something went wrong...";
+    }
+});
